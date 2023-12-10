85b47fed940464af6c8e6179c9dc6cf665c54f48
Emile Rolley <emile.rolley@tuta.io>
2023-12-10 23:33:12 +0100

    nitpicks

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,5 @@
 module Main exposing (..)
 
--- Press buttons to increment and decrement a counter.
---
--- Read how it works:
---   https://guide.elm-lang.org/architecture/buttons.html
---
-
 import Browser
 import Dict
 import Effect
@@ -203,7 +197,7 @@ viewTotal total =
                 H.text "empty"
 
             Nothing ->
-                H.text "..."
+                H.text "_"
         ]
 
 

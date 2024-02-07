a3c658c9dee6c082bb5b305eec4e0f4b8a034991
Emile Rolley <emile.rolley@tuta.io>
2024-02-07 10:53:16 +0100

    ui: setup custom font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ export default {
     content: ["./index.html", "./src/**/*.elm"],
     theme: {
         extend: {
+            fontSize: {
+                xs: ".75rem",
+                sm: ".875rem",
+                tiny: ".875rem",
+                base: "1rem",
+                lg: "1.125rem",
+                xl: "1.25rem",
+                "2xl": "1.5rem",
+                "3xl": "1.875rem",
+                "4xl": "2.25rem",
+            },
             fontFamily: {
                 sans: [
                     /* "Syne", "Istok Web",  */ "Titillium Web",

81bee4f4a447268bddafb7e05d3ee6472c8cf539
Clément <clem.auger@hotmail.fr>
2024-02-18 11:16:37 +0100

    fix: disable hover effect on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.elm"],
+    future: {
+        hoverOnlyWhenSupported: true,
+    },
     theme: {
         extend: {
             fontSize: {

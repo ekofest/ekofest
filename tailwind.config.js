4a288339b777ffbbd04e94412eb83d97825340ad
Clément <clem.auger@hotmail.fr>
2024-03-31 20:17:20 +0200

    feat: improve doc style for blockquote

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,29 @@ export default {
                 sans: ["Titillium Web", "sans-serif"],
             },
         },
+        typography: {
+            DEFAULT: {
+                css: {
+                    blockquote: {
+                        "font-style": "normal",
+                        "border-left": "none",
+                        "padding-left": "0",
+                        "border-radius": "1rem",
+                        padding: "0.5rem",
+                        "background-color": "grey",
+                        p: {
+                            margin: "0.5rem",
+                        },
+                        "p:first-of-type::before": {
+                            content: "none",
+                        },
+                        "p:first-of-type::after": {
+                            content: "none",
+                        },
+                    },
+                },
+            },
+        },
     },
     plugins: [require("@tailwindcss/typography"), require("daisyui")],
     daisyui: {
